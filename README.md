66c1f0b68ae2e3ac9439d87abc26f2d08e9f4438
Alexander Christie <alex@briefly.io>
2015-09-16 22:53:11 +0100

    Initial Version
    Signed-off-by: Alexander Christie <alex@briefly.io>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Amp Format
+
+Amp is the format Briefly uses to enclose it's WebSocket transmissions.
+
+## Testing
+
+This module is so primitive as to not require tests. 
---
+++ src/index.js
@@ -0,0 +1,13 @@
+/*
+    MIT License - YAY
+ */
+
+module.exports = function (type, data) {
+
+    return {
+        format: 'briefly',
+        type: type,
+        data: data
+    }
+
+}
